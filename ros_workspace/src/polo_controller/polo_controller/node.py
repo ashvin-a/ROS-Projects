bddcf18e346aa5e475983632b8782bb1deb19d3b
= <=>
2025-05-08 21:03:38 +0530

    Add some code to learn timer.

--- ros_workspace/src/polo_controller/polo_controller/node.py
+++ ros_workspace/src/polo_controller/polo_controller/node.py
@@ -7,6 +7,10 @@ class Thala(Node):
     def __init__(self):
         super().__init__("thala_node")
         self.get_logger().info("Im alive!")
+        self.create_timer(timer_period_sec=1, callback=self.timer)
+    
+    def timer(self):
+        self.get_logger().info("Hello human!")
 
 def main(args=None):
 
