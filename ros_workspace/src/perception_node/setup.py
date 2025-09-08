22a3f4381663eea9db0a020542caa0a0bbd6a3e9
ashvin <ashvinanilkumarsh@gmail.com>
2025-09-08 12:18:18 -0500

    Fix typo in setup.py

--- ros_workspace/src/perception_node/setup.py
+++ ros_workspace/src/perception_node/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'perception_node = perceprion_node.node:main'
+            'perception_node = perception_node.node:main'
         ],
     },
 )
